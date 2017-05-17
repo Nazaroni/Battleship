b9a77421aba1aad5df011c10b71e923836fc89ec
Nazar Malyy <nazar.malyy@gmail.com>
2017-05-17 23:19:40 +0200

    Add new code, now game ready to play

--- Battleship1.py
+++ Battleship1.py
@@ -20,37 +20,75 @@ def random_col(board):
 
 ship_row = random_row(board)
 ship_col = random_col(board)
-print ship_row
-print ship_col
+#print ship_row
+#print ship_col
 
 turn = 0
 
 for turn in range(4):
 
-    turn += 1
-
-
     guess_row = int(raw_input("Guess Row: "))
     guess_col = int(raw_input("Guess Col: "))
 
 
-    #This checks if you guessed correctly
+    #This checks if you guessed correctly.
     if guess_row == ship_row and guess_col == ship_col:
         print "Congratulation! You sank my battleship"
+        break
 
     else:
 
-        if guess_col and guess_row not in range(0, len(board) -1):
+        #This checks if you guessed within the 5x5 grid.
+        if guess_col not in range(0, len(board)) or guess_row not in range(0, len(board)):
             print "Oops, that's not even in the ocean."
 
+        #This checks if you have already guessed in that location.
         elif board[guess_col][guess_row] == "X":
             print "You guessed that one already."
 
+        #This tell you, you missed and it marks that spot as "X".
         else:
             print "You missed my battleship!"
             board[guess_col][guess_row] = "X"
 
+        turn += 1
         print "Your turn is %s" % turn
+
+        #TODO: delete this turn in final code
         print turn
         print_board(board)
 
+        if turn == 4:
+            print "Game is over! You have run out of turns."
+            print "Game Over"
+
+'''
+
+
+You can also add on to your Battleship! program to make it more complex and fun to play.
+Here are some ideas for enhancements—maybe you can think of some more!
+
+1.
+    Make multiple battleships: you'll need to be careful because you need to make sure
+    that you don’t place battleships on top of each other on the game board.
+    You'll also want to make sure that you balance the size of the board with
+    the number of ships so the game is still challenging and fun to play.
+
+2.
+    Make battleships of different sizes: this is trickier than it sounds.
+    All the parts of the battleship need to be vertically or horizontally
+    touching and you’ll need to make sure you don’t accidentally place part
+    of a ship off the side of the board.
+
+3.
+    Make your game a two-player game.
+
+4.
+    Use functions to allow your game to have more features like rematches, statistics and more!
+
+Some of these options will be easier after we cover loops in the next lesson.
+Think about coming back to Battleship! after a few more lessons
+and see what other changes you can make!
+
+
+'''
